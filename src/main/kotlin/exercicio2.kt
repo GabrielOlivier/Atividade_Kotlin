e555c4735b0142ae6990d92f10bd85707b4344fc
GabrielOlivier <57776304+GabrielOlivier@users.noreply.github.com>
2021-03-18 13:13:15 -0300

    Initial commit

---
+++ src/main/kotlin/exercicio2.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+   var  result = nomeCompleto("Gabriel", "Dorival de Lima", "Oliveira")
+    println(result)
+
+}
+
+fun nomeCompleto(primeiroNome: String, meioNome: String, finalNome: String) {
+    print("Meu nome completo é $primeiroNome $meioNome $finalNome");
+}
